cf7138753565be4e41f7861fe2458d3b4411f0f2
z5256989 <z5256989@ad.unsw.edu.au>
2020-09-26 23:01:11 +1000

    first version of channels test

---
+++ src/channels_test_phillip_steven.py
@@ -0,0 +1,24 @@
+'''
+26/9/2020
+Phillip Law and Steven Sok
+
+Purpose: Test functions in channels.py
+'''
+import pytest
+import channel
+import channels
+from error import InputError
+
+'''
+#
+def test_channels_list():
+    channels
+
+def test_channels_listall():
+
+
+def test_channels_create():
+
+
+
+'''
