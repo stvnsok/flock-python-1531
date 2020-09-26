7f07eb3039c5cc1fc97476f318d26eab34fa8b02
Marko Wong <z5309371@ad.unsw.edu.au>
2020-09-26 20:39:32 +1000

    channels_create done

--- src/channels.py
+++ src/channels.py
@@ -1,3 +1,8 @@
+from data import *
+import re
+import pytest
+from error import InputError
+
 def channels_list(token):
     return {
         'channels': [
@@ -19,12 +24,59 @@ def channels_listall(token):
     }
 
 def channels_create(token, name, is_public):
+    # Get users from data
+    users = data['users']
 
+    # Check that token exists
+    valid_token = 0
+    for user in users:
+        if (user['token'] == token):
+            valid_token = 1
+            break
+    
+    # raise error for invalid token
+    if valid_token == 0 and len(users) is not 0:
+        raise InputError('invalid token')
+    
+    # raise error for name being too long
+    if len(name) > 20:
+        raise InputError('Name too long')
+
+    # Grabs all channels from data
+    channels = data['channels']
+
+    # Counting the number of private and public channels
+    private_channel_count = 0
+    public_channel_count = 0
+    for channel in channels:
+        if channel['channel_id'] >= 9000 and  channel['channel_id'] < 10000:
+            private_channel_count =+ 1
+        elif channel['channel_id'] >= 1000 and channel['channel_id'] < 2000:
+            public_channel_count =+ 1
+    
+    # Generate channel id
+    if is_public is True:
+        channel_id = 1000 + public_channel_count
+    else:
+        channel_id = 9000 + private_channel_count
+
+    # Creating a new dictornary for new channel
     new_channel = {
-        'channel_id': len(channels)
-        ''
+        'channel_id': channel_id,
+        'name': name,
     }
+
+    # Adding user to dictionary
+    channels.append(new_channel)
+
+    print(channels)
 
     return {
-        'channel_id': 1,
+        'channel_id': new_channel['channel_id'],
+        'name': new_channel['name'],
     }
+
+channels_create("token", "channel_1", True)
+channels_create("token", "channel_2", False)
+channels_create("token", "channel_3", False)
+channels_create("token", "channel_4", True)
