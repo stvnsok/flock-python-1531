78cfffac6d4d0cb6f712936499a4ecb6856aefba
z5256989 <z5256989@ad.unsw.edu.au>
2020-09-29 16:51:17 +1000

    updated the functions file with some description

--- src/channels.py
+++ src/channels.py
@@ -1,3 +1,11 @@
+'''
+channels_list
+Provide a list of all channels (and their associated details) that the authorised user is part of
+Parameters: (token)
+Return type: {channels}
+Exceptions: N/A
+'''
+
 def channels_list(token):
     return {
         'channels': [
@@ -8,6 +16,14 @@ def channels_list(token):
         ],
     }
 
+'''
+channels_listall
+Provide a list of all channels (and their associated details)
+Parameters: (token)
+Return type: {channels}
+Exceptions: N/A
+'''
+
 def channels_listall(token):
     return {
         'channels': [
@@ -17,6 +33,14 @@ def channels_listall(token):
         	}
         ],
     }
+
+'''
+channels_create
+Creates a new channel with that name that is either a public or private channel
+Parameters: (token, name, is_public)
+Return type: {channel_id}
+Exceptions: InputError when the name of the channel exceeds 20 characters
+'''
 
 def channels_create(token, name, is_public):
     return {
