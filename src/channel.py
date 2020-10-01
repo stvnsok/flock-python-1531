36acd1e31564ecc41e3f2546cfd4c33da1d904c9
Jay Anand <z5059370@ad.unsw.edu.au>
2020-10-01 12:34:40 +1000

    Finished channnel_messages function for channel.py

--- src/channel.py
+++ src/channel.py
@@ -1,5 +1,5 @@
 from data import *
-from error import *
+from error import InputError, AccessError
 
 
 def channel_invite(token, channel_id, u_id):
@@ -76,34 +76,63 @@ def channel_details(token, channel_id):
 
 
 def channel_messages(token, channel_id, start):
+    '''Given a Channel with ID channel_id that the authorised user is part of, 
+    return up to 50 messages between index "start" and "start + 50". Message with 
+    index 0 is the most recent message in the channel. This function returns a new 
+    index "end" which is the value of "start + 50", or, if this function has returned 
+    the least recent messages in the channel, returns -1 in "end" to indicate there are
+    no more messages to load after this return.'''
 
+    channels = data['channels']
+    users = data['users']
+
+    # Finds the Channel, if it doesn't exists assigns None
+    channel = next(
+        (channel for channel in channels if channel['id'] == channel_id), None)
+
+    # Get the user that is sending the request
+    authorised_user = next(
+        (user for user in users if user['token'] == token), None)
+
+    # Check if channel exists
+    if (channel == None):
+        raise InputError('Channel_id does not exist')
+
+    # Check if authorised user is a member of the channel
+    if not any(authorised_user['u_id'] == member['u_id'] for member in channel['all_members']):
+        raise AccessError(
+            'Authorised user is not a member of the channel')
+
+    # sets the index for the last message
+    end = start + 50
+    messages = channel['messages']
+
+    # if this index is greater than the amount of messages then
+    # the amount of remaining messages after the start index
+    # is less than 50
+    if (len(channel['messages']) < end):
+        end = -1
+        messages = messages[start:]
+    # send back messages between the start and end range
+    else:
+        messages = messages[start: end]
+
+    # return result
     return {
-        'messages': [
-            {
-                'message_id': 1,
-                'u_id': 1,
-                'message': 'Hello world',
-                'time_created': 1582426789,
-            }
-        ],
-        'start': 0,
-        'end': 50,
+        'messages': messages,
+        'start': start,
+        'end': end,
     }
 
 
-'''Given a Channel with ID channel_id that the authorised user is part of, 
-return up to 50 messages between index "start" and "start + 50". Message with 
-index 0 is the most recent message in the channel. This function returns a new 
-index "end" which is the value of "start + 50", or, if this function has returned 
-the least recent messages in the channel, returns -1 in "end" to indicate there are
-no more messages to load after this return.'''
-
 # Given a channel ID, the user removed as a member of this channel
+
+
 def channel_leave(token, channel_id):
-    
+
     channels = data['channels']
     users = data['users']
-    
+
     # Followed Jays format for error testing
     channel = next(
         (channel for channel in channels if channel['id'] == channel_id), None)
@@ -117,33 +146,35 @@ def channel_leave(token, channel_id):
     if not any(authorised_user['u_id'] == member['u_id'] for member in channel['all_members']):
         raise AccessError(
             'Authorised user is not a member of this channel')
-    
-    member_number = 0            
+
+    member_number = 0
     # Removal users from member -> make it more 'pythonic'?
     for member in channel['all_members']:
         if authorised_user['u_id'] == member['u_id']:
             break
-        member_number += 1 
-    
+        member_number += 1
+
     channel['all_members'].pop(member_number)
 
     # Remove the user from owners
     for member in channel['owner_members']:
         if authorised_user['u_id'] == member['u_id']:
             break
-        member_number += 1 
-    
+        member_number += 1
+
     channel['owner_members'].pop(member_number)
 
     return {}
 
 # Given a channel_id of a channel that the authorised user can join,
 # adds them to that channel
+
+
 def channel_join(token, channel_id):
-    
+
     channels = data['channels']
     users = data['users']
-    
+
     channel = next(
         (channel for channel in channels if channel['id'] == channel_id), None)
 
@@ -152,77 +183,81 @@ def channel_join(token, channel_id):
 
     if (channel == None):
         raise InputError('Channel_id does not exist')
-    
+
     if (channel['id'] >= 9000):
         raise AccessError(
-            'Authorised member is not owner of selected private channel')   
-    
+            'Authorised member is not owner of selected private channel')
+
     channel['all_members'].append(authorised_user)
-     
+
     return {}
-    
+
 # Make user with user id u_id an owner of this channel
+
+
 def channel_addowner(token, channel_id, u_id):
 
     channels = data['channels']
     users = data['users']
-    
+
     channel = next(
         (channel for channel in channels if channel['id'] == channel_id), None)
 
     user = next(
         (user for user in users if user['u_id'] == u_id), None)
-    
+
     authorised_user = next(
-        (user for user in users if user['token'] == token), None)    
-       
+        (user for user in users if user['token'] == token), None)
+
     if any(user['u_id'] == member['u_id'] for member in channel['owner_members']):
         raise InputError(
-            'User is already an owner of the channel')  
+            'User is already an owner of the channel')
 
     if (channel == None):
         raise InputError('Channel_id does not exist')
 
-    if not any(authorised_user['u_id'] == member['u_id'] for member in channel['owner_members']):
-        raise AccessError(
-            'Authorised user is not an owner of the channel')  
-    
-    channel['owner_members'].append(user)
-    
-    return {}
-
-# Remove user with user id u_id an owner of this channel
-def channel_removeowner(token, channel_id, u_id):
-   
-    channels = data['channels']
-    users = data['users']
-    
-    channel = next(
-        (channel for channel in channels if channel['id'] == channel_id), None)
-
-    user = next(
-        (user for user in users if user['u_id'] == u_id), None)
-    
-    authorised_user = next(
-        (user for user in users if user['token'] == token), None)    
-         
-    if (channel == None):
-        raise InputError('Channel_id does not exist')
-    
-    if not any(user['u_id'] == member['u_id'] for member in channel['owner_members']):
-        raise InputError(
-            'User is not an owner of the channel')
-            
     if not any(authorised_user['u_id'] == member['u_id'] for member in channel['owner_members']):
         raise AccessError(
             'Authorised user is not an owner of the channel')
-    
+
+    channel['owner_members'].append(user)
+
+    return {}
+
+# Remove user with user id u_id an owner of this channel
+
+
+def channel_removeowner(token, channel_id, u_id):
+
+    channels = data['channels']
+    users = data['users']
+
+    channel = next(
+        (channel for channel in channels if channel['id'] == channel_id), None)
+
+    user = next(
+        (user for user in users if user['u_id'] == u_id), None)
+
+    authorised_user = next(
+        (user for user in users if user['token'] == token), None)
+
+    if (channel == None):
+        raise InputError('Channel_id does not exist')
+
+    if not any(user['u_id'] == member['u_id'] for member in channel['owner_members']):
+        raise InputError(
+            'User is not an owner of the channel')
+
+    if not any(authorised_user['u_id'] == member['u_id'] for member in channel['owner_members']):
+        raise AccessError(
+            'Authorised user is not an owner of the channel')
+
     member_number = 0
     for member in channel['owner_members']:
         if user['u_id'] == member['u_id']:
             break
-        member_number += 1 
+        member_number += 1
 
     channel['owner_members'].pop(member_number)
-    
-    return {}  
+
+    return {}
