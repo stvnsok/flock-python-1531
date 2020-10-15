ec606946ea3222465a117dfacb33598632935467
Jay Anand <z5059370@ad.unsw.edu.au>
2020-10-15 21:31:27 +1100

    Fixed pylint errors in auth.py

--- src/auth.py
+++ src/auth.py
@@ -1,24 +1,36 @@
-from data import data
+'''
+import regex library
+'''
 import re
-import pytest
+from data import data
 from error import InputError
 
+
 def create_token(email):
+    '''
+    Creates a token for each user
+    '''
     # creates a hash using in built python hash function
     return str(hash(email))
 
 
 def check(email):
+    '''
+    Checks that the entered email is valid based on regex expression
+    '''
     regex = r'^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
-    
+
     # Make a regular expression for validating an Email
-    if(re.search(regex, email)):
+    if re.search(regex, email):
         return True
-    else:
-        return False
+
+    return False
 
 
 def auth_login(email, password):
+    '''
+    Login and authenticate existing user
+    '''
 
     # Check email is valid format
     if (check(email)) is not True:
@@ -27,13 +39,13 @@ def auth_login(email, password):
     # Get users from data
     users = data['users']
 
-    # Check that email and password are correct and valid    
+    # Check that email and password are correct and valid
     user = next((user for user in users if user['email'] == email), None)
 
     # If the was not found based on email, throw exception
-    if user == None:
+    if user is None:
         raise InputError('Incorrect email')
-    
+
     # If password matches send back id and token
     # Else throw exception
     if user['password'] == password:
@@ -41,23 +53,29 @@ def auth_login(email, password):
             'u_id': user['u_id'],
             'token': user['token']
         }
-    else:
-        raise InputError('Incorrect password')
-    
+
+    raise InputError('Incorrect password')
+
 
 def auth_logout(token):
+    '''
+    Logout authenticated user
+    '''
 
     # Get users from data
     users = data['users']
 
     # Check that token exists
     if any(user['token'] == token for user in users):
-        return {'is_success' : True}
-    else:
-        return {'is_success': False}
+        return {'is_success': True}
+
+    return {'is_success': False}
 
 
 def auth_register(email, password, name_first, name_last):
+    '''
+    Register a new user
+    '''
 
     # checks for valid email
     if check(email) is not True:
@@ -88,7 +106,6 @@ def auth_register(email, password, name_first, name_last):
     if len(handle) > 20:  # keeping the handle under 20 chars
         handle = handle[0:20]
 
-
     # Creating a new dictionary for new user
     new_user = {
         'u_id': len(users),
@@ -105,7 +122,7 @@ def auth_register(email, password, name_first, name_last):
         new_user['u_id'] = 1
     else:
         new_user['u_id'] = users[-1]['u_id'] + 1
-    
+
     # Adding user to dictionary
     users.append(new_user)
 
