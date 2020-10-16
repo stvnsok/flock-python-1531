d522d86bf4d8e46e3aeb27f718321f40e830f3d6
Marko Wong <z5309371@ad.unsw.edu.au>
2020-10-16 15:37:34 +1100

    setup user_test file and user_marko file

---
+++ src/user_marko.py
@@ -0,0 +1,25 @@
+from data import data
+from error import InputError, AccessError
+
+def user_profile(token, u_id):
+    return {
+        'user': {
+        	'u_id': 1,
+        	'email': 'cs1531@cse.unsw.edu.au',
+        	'name_first': 'Hayden',
+        	'name_last': 'Jacobs',
+        	'handle_str': 'hjacobs',
+        },
+    }
+
+def user_profile_setname(token, name_first, name_last):
+    return {
+    }
+
+def user_profile_setemail(token, email):
+    return {
+    }
+
+def user_profile_sethandle(token, handle_str):
+    return {
+    }
---
+++ src/user_test.py
@@ -0,0 +1,11 @@
+'''
+Tests for user.py
+
+'''
+
+import pytest
+import user
+from error import InputError
+from other import clear
+from data import data
+
