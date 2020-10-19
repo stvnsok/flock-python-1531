61ce8170f6c4f2c0a9444c1699f7c3a0d5c9a076
Jay Anand <z5059370@ad.unsw.edu.au>
2020-10-19 16:17:18 +1100

    Moved check email function into other

--- src/other.py
+++ src/other.py
@@ -6,6 +6,7 @@ import calendar
 def clear():
     data['users'].clear()
     data['channels'].clear()
+    data['messages'].clear()
 
 
 def users_all(token):
@@ -37,5 +38,20 @@ def search(token, query_str):
     }
 
 def get_timestamp():
+    '''
+    Returns a unix timestamp
+    '''
     d = datetime.utcnow()
     return calendar.timegm(d.utctimetuple())
+
+def check(email):
+    '''
+    Checks that the entered email is valid based on regex expression
+    '''
+    regex = r'^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
+
+    # Make a regular expression for validating an Email
+    if re.search(regex, email):
+        return True
+
+    return False
