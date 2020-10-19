edbd28d9d393e18df95d472ec277e94f9bf16eec
Jay Anand <z5059370@ad.unsw.edu.au>
2020-10-19 21:37:00 +1100

    Made changes to message send and message remove to reflect new structure of data dict

--- src/message.py
+++ src/message.py
@@ -18,17 +18,19 @@ def message_send(token, channel_id, message):
     if len(message) > 1000:
         raise InputError("Message is more than 1000 characters")
 
+    # Get the selected channel
+    channel = next(channel for channel in data['channels'] if channel['channel_id'] == channel_id, None)
+
     # Create new message object
     new_message = {
         'message_id' : str(uuid.uuid4()),
         'message' : message,
         'u_id': authorised_user['u_id'],
         'time_created': get_timestamp()
-        'channel_id': channel_id
     }   
 
     # Add new message to dictionary
-    data['messages'].append(new_message)
+    channel['messages'].append(new_message)
 
     return {
         'message_id': new_message['message_id'],
@@ -42,31 +44,25 @@ def message_remove(token, message_id):
     # Get the user that is sending the request
     authorised_user = next(
         (user for user in users if user['token'] == token), None)
+
+    # Get the channel where the message exists
+    channel = next(channel for channel in channels for message in channel['messages'] if message['message_id'] == message_id)
+
+    # Check if authorised user is owner
+    channel_owner = any(member['permission_id'] == 1 and member['u_id'] == authorised_user['u_id'] for member in channel['members'])
     
-    # Get the mesage from messages
-    message = next(message for message in messages if message['message_id'] == message_id, None);
-
-    # If message does not exist throw error
-    if message is None:
-        raise InputError("Message does not exist")
-
-    # Get the channel where this message exists    
-    channel = next(channel for channel in channels if channel['channel_id'] = message['channel_id'])
-
-    # Determine if authorised user is an owner of the channel where message was sent
-    channel_owner = any(authorised_user['u_id'] == member['u_id'] and member['is_owner'] for member in channel['members'])
-
-    # Throw error if not owner
-    if not channel_owner:
-        raise AccessError("Message to remove was not sent by an owner of this channel")
-
-    # Throw error if the message was not sent by the authorised user 
-    if not channel_owner and message['u_id'] != authorised_user['u_id']:
-        raise AccessError("Message to remove was not sent by authorised user")
-    
-    # Remove message and return
-    messages.remove(message)
-    return {}
+    # Check the messages in the channel
+    # Throw an access error if the message was not sent by authorised user, 
+    # or authorised user is not an owner of the channel.
+    for message in channel['messages']:
+        if message['message_id'] == message_id:
+            if message['u_id'] == authorised_user['u_id'] or channel_owner:
+                channel['messages'].remove(message)
+                return {}
+            elif not channel_owner:
+                raise AccessError("Authorised user is not an owner of the channel")
+            else:
+                raise AccessError("Message to remove was not sent by authorised user")
     
 
 def message_edit(token, message_id, message):
