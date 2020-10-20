c008748495d22a2507d758d1e0010512bd45c925
Jay Anand <z5059370@ad.unsw.edu.au>
2020-10-20 18:17:38 +1100

    completed all functions for helper functions. Use this as it will make it really easy to just convert our existing tests to http tests. Check auth_test to see how I use the helper_test functions

--- src/auth_test.py
+++ src/auth_test.py
@@ -15,18 +15,7 @@ import requests
 import json
 import pytest
 import server
-from helper_test_functions import *
-
-# # Helper function to set up user
-# def register_user(email, password, name_first, name_last, url):
-#     response = requests.post(f'{url}auth/register', json={
-#         "email": "john@gmail.com", 
-#         "password": "qwe123!@#",
-#         "name_first": "John",
-#         "name_last": "Smith",
-#     })
-
-#     return response.json()
+import helper_test_functions as test_setup
 
 # Fixture to get the URL of the server. 
 @pytest.fixture
@@ -57,15 +46,15 @@ def test_successful_registration(url):
     '''
     Test registration, login and logout working
     '''
-    register_user("john@gmail.com", "qwe123!@#", "John", "Smith", url)
+    test_setup.register_user("john@gmail.com", "qwe123!@#", "John", "Smith", url)
     
-    login_response = login_user("john@gmail.com", "qwe123!@#", url)
+    login_response = test_setup.login_user("john@gmail.com", "qwe123!@#", url)
 
-    logout_response = logout_user(login_response["token"], url) 
+    logout_response = test_setup.logout_user(login_response["token"], url) 
 
     assert logout_response["is_success"] == True
     
-    requests.delete(f'{url}/clear')
+    test_setup.clear(url)
 
 
 
@@ -74,12 +63,12 @@ def test_invalid_email(url):
     Test that InputError is thrown for invalid email input
     '''
 
-    error_response = register_user("john.com", "qwe123!@#" , "John", "Smith", url)
+    error_response = test_setup.register_user("john.com", "qwe123!@#" , "John", "Smith", url)
 
     assert error_response["code"] == 400
     assert error_response["message"] == "<p>Invalid email</p>"
 
-    requests.delete(f'{url}/clear')
+    test_setup.clear(url)
 
 
 def test_email_already_registered(url):
@@ -88,14 +77,14 @@ def test_email_already_registered(url):
     with email that has already been registered
     '''
 
-    register_user("john@gmail.com", "qwe123!@#", "John", "Smith", url)
+    test_setup.register_user("john@gmail.com", "qwe123!@#", "John", "Smith", url)
     
-    error_response = register_user("john@gmail.com", "qwe123!@#", "John", "Smith", url)
+    error_response = test_setup.register_user("john@gmail.com", "qwe123!@#", "John", "Smith", url)
 
     assert error_response["code"] == 400
     assert error_response["message"] == "<p>Email already registered</p>"
 
-    requests.delete(f'{url}/clear')
+    test_setup.clear(url)
 
 
 def test_invalid_password(url):
@@ -104,12 +93,12 @@ def test_invalid_password(url):
     is too short (less than 6 characters)
     '''
     
-    error_response = register_user("john@gmail.com", "qwe",  "John", "Smith", url)
+    error_response = test_setup.register_user("john@gmail.com", "qwe",  "John", "Smith", url)
 
     assert error_response["code"] == 400
     assert error_response["message"] == "<p>Password too short</p>"
 
-    requests.delete(f'{url}/clear')
+    test_setup.clear(url)
 
 
 
@@ -124,18 +113,18 @@ def test_invalid_name_first(url):
                         adklfhasklhfklsahfklsadhfklasdhfklasdhfkljs"""
 
     
-    error_response = register_user("john@gmail.com","qwe123!@#", long_first_name, "Smith" ,url)
+    error_response = test_setup.register_user("john@gmail.com","qwe123!@#", long_first_name, "Smith" ,url)
 
     assert error_response["code"] == 400
     assert error_response["message"] == "<p>First Name too long or short</p>"
 
 
-    error_response = register_user("john@gmail.com","qwe123!@#", "", "Smith" ,url)
+    error_response = test_setup.register_user("john@gmail.com","qwe123!@#", "", "Smith" ,url)
 
     assert error_response["code"] == 400
     assert error_response["message"] == "<p>First Name too long or short</p>"
 
-    requests.delete(f'{url}/clear')
+    test_setup.clear(url)
 
 
 def test_invalid_name_last(url):
@@ -149,17 +138,17 @@ def test_invalid_name_last(url):
                         adklfhasklhfklsahfklsadhfklasdhfklasdhfkljs"""
 
     
-    error_response = register_user("john@gmail.com","qwe123!@#", "John", long_last_name ,url)
+    error_response = test_setup.register_user("john@gmail.com","qwe123!@#", "John", long_last_name ,url)
 
     assert error_response["code"] == 400
     assert error_response["message"] == "<p>Last Name too long or short</p>"
 
-    error_response = register_user("john@gmail.com","qwe123!@#", "John", "" ,url)
+    error_response = test_setup.register_user("john@gmail.com","qwe123!@#", "John", "" ,url)
 
     assert error_response["code"] == 400
     assert error_response["message"] == "<p>Last Name too long or short</p>"
 
-    requests.delete(f'{url}/clear')    
+    test_setup.clear(url)
 
 
 
@@ -169,12 +158,12 @@ def test_incorrect_email_login(url):
     registered yet
     '''
 
-    error_response = login_user("bob@gmail.com","qwe123!@#", url)
+    error_response = test_setup.login_user("bob@gmail.com","qwe123!@#", url)
 
     assert error_response["code"] == 400
     assert error_response["message"] == "<p>Email does not belong to a user</p>"
 
-    requests.delete(f'{url}/clear')    
+    test_setup.clear(url)
 
 
 
@@ -183,25 +172,14 @@ def test_incorrect_password_login(url):
     Test that InputError is throw for login where password
     is incorrect
     '''
-    
-    requests.post(f'{url}auth/register', json={
-        "email": "john@gmail.com", 
-        "password": "qwe123!@#",
-        "name_first": "John",
-        "name_last": "Smith",
-    })
+    test_setup.register_user("john@gmail.com", "qwe123!@#", "John", "Smith", url)
 
-    response = requests.post(f'{url}auth/login', json={
-        "email": "john@gmail.com", 
-        "password": "qwe12",
-    })
-
-    error_response = response.json()
+    error_response = test_setup.login_user("john@gmail.com", "qwe12", url)
 
     assert error_response["code"] == 400
     assert error_response["message"] == "<p>Incorrect password</p>"
 
-    requests.delete(f'{url}/clear')  
+    test_setup.clear(url)
 
 
 def test_logout_fail(url):
@@ -210,31 +188,31 @@ def test_logout_fail(url):
     correct error is passed to user
     '''
 
-    register_user("john@gmail.com", "qwe123!@#", "John", "Smith", url)
+    test_setup.register_user("john@gmail.com", "qwe123!@#", "John", "Smith", url)
 
-    login_user("john@gmail.com", "qwe123!@#", url)
+    test_setup.login_user("john@gmail.com", "qwe123!@#", url)
 
-    logout_response = logout_user("WrongToken", url)
+    logout_response = test_setup.logout_user("WrongToken", url)
 
     assert logout_response["is_success"] == False
     
-    requests.delete(f'{url}/clear')
+    test_setup.clear(url)
 
 def test_handle_too_long(url):
     '''
     Test that handle is being created correctly
     '''
 
-    register_response = register_user("john@gmail.com", "qwe123!@#", "1234567890", "yoyoy123456789", url)
+    register_response = test_setup.register_user("john@gmail.com", "qwe123!@#", "1234567890", "yoyoy123456789", url)
 
-    response = requests.get(f'{url}users/all', json={"token":register_response["token"]})
+    # response = requests.get(f'{url}users/all', json={"token":register_response["token"]})
 
-    usersall_response = response.json()
+    usersall_response = test_setup.users_all(register_response["token"], url)
 
     handle = usersall_response["users"][0]["handle_str"]
 
     assert handle == "1234567890yoyoy12345"
 
-    requests.delete(f'{url}/clear')
+    test_setup.clear(url)
 
     
--- src/helper_test_functions.py
+++ src/helper_test_functions.py
@@ -6,6 +6,11 @@ These are helper functions that will help reduce a lot of boilerplate
 in testing. Enjoy :D
 '''
 
+
+'''
+---------------------Auth Functions---------------------
+'''
+
 def register_user(email, password, name_first, name_last, url):
     response = requests.post(f'{url}auth/register', json={
         "email": email, 
@@ -32,3 +37,198 @@ def logout_user(token, url):
     return response.json()
 
 
+
+'''
+---------------------Channel Functions---------------------
+'''
+
+def channel_invite(token, channel_id, u_id, url):
+    response = requests.post(f'{url}channel/invite', json={
+        "token" : token,
+        "channel_id": channel_id,
+        "u_id": u_id
+    })
+
+    return response.json()
+
+def channel_details(token, channel_id, url):
+    response = requests.get(f'{url}channel/details', json={
+        "token" : token,
+        "channel_id": channel_id,
+    })
+
+    return response.json()
+
+def channel_messages(token, channel_id, start, url):
+    response = requests.get(f'{url}channel/messages', json={
+        "token" : token,
+        "channel_id": channel_id,
+    })
+
+    return response.json()
+
+def channel_leave(token, channel_id, url):
+    response = requests.post(f'{url}channel/leave', json={
+        "token" : token,
+        "channel_id": channel_id,
+    })
+
+    return response.json()
+
+def channel_join(token, channel_id, url):
+    response = requests.post(f'{url}channel/join', json={
+        "token" : token,
+        "channel_id": channel_id,
+    })
+
+    return response.json()
+
+def channel_addowner(token, channel_id, u_id, url):
+    response = requests.post(f'{url}channel/addowner', json={
+        "token" : token,
+        "channel_id": channel_id,
+        "u_id": u_id
+
+    })
+
+    return response.json()
+
+def channel_removeowner(token, channel_id, u_id, url):
+    response = requests.post(f'{url}channel/removeowner', json={
+        "token" : token,
+        "channel_id": channel_id,
+        "u_id": u_id
+
+    })
+
+    return response.json()
+
+
+'''
+---------------------Channels Functions---------------------
+'''
+
+def channels_list(token, url):
+    response = requests.get(f'{url}channel/list', json={
+        "token" : token,
+    })
+
+    return response.json()
+
+def channels_listall(token, url):
+    response = requests.get(f'{url}channel/listall', json={
+        "token" : token,
+    })
+
+    return response.json()
+
+
+def channels_create(token, name, is_public, url):
+    response = requests.post(f'{url}channels/create', json={
+        "token" : token,
+        "name" : name,
+        "is_public" : is_public
+    })
+
+    return response.json()
+
+'''
+---------------------Message Functions---------------------
+'''
+
+def message_send(token, channel_id, message, url):
+    response = requests.post(f'{url}message/send', json={
+        "token" : token,
+        "channel_id" : channel_id,
+        "is_public" : is_public
+    })
+
+    return response.json()
+
+
+def message_remove(token, message_id, url):
+    response = requests.delete(f'{url}message/remove', json={
+        "token" : token,
+        "message_id" : message_id,
+    })
+
+    return response.json()
+
+
+def message_edit(token, message_id, message, url):
+    response = requests.put(f'{url}message/edit', json={
+        "token" : token,
+        "channel_id" : message_id,
+        "message" : message
+    })
+
+    return response.json()
+
+'''
+---------------------User Functions---------------------
+'''
+
+def user_profile(token, u_id, url):
+    response = requests.get(f'{url}user/profile', json={
+        "token" : token,
+        "u_id" : u_id,
+    })
+
+    return response.json()
+
+def user_profile_setname(token, name_first, name_last, url):
+    response = requests.put(f'{url}user/profile/setname', json={
+        "token" : token,
+        "name_first" : name_first,
+        "name_last" : name_last,
+    })
+
+    return response.json()
+
+def user_profile_setemail(token, email, url):
+    response = requests.put(f'{url}user/profile/setemail', json={
+        "token" : token,
+        "email" : email
+    })
+
+    return response.json()
+
+def user_profile_sethandle(token, handle_str, url):
+    response = requests.put(f'{url}user/profile/sethandle', json={
+        "token" : token,
+        "handle_str" : handle_str
+    })
+
+    return response.json()
+
+
+'''
+---------------------Other Functions---------------------
+'''
+
+def users_all(token, url):
+    response = requests.get(f'{url}users/all', json={
+        "token" : token,
+    })
+
+    return response.json()
+
+def change_userpermission(token, u_id, permission_id, url):
+    response = requests.post(f'{url}admin/userpermission/change', json={
+        "token" : token,
+        "u_id" : u_id,
+        "permission_id" : permission_id
+    })
+
+    return response.json()
+
+def search(token, query_str, url):
+    response = requests.get(f'{url}search', json={
+        "token" : token,
+        "query_str" : query_str
+    })
+
+    return response.json()
+
+def clear(url):
+    requests.delete(f'{url}clear')
