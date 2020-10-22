dfb35e91324fde32377863de210f394bf96b6291
Jay Anand <z5059370@ad.unsw.edu.au>
2020-10-22 11:06:39 +1100

    Auth_tests complete

--- src/auth_test.py
+++ src/auth_test.py
@@ -2,22 +2,16 @@
 Tests for user.py
 
 '''
-import pytest
-import auth
-from error import InputError
-from other import clear
-from data import data
 import re
 from subprocess import Popen, PIPE
 import signal
 from time import sleep
-import requests
-import json
 import pytest
-import server
 import helper_test_functions as test_setup
 
-# Fixture to get the URL of the server. 
+# Fixture to get the URL of the server.
+
+
 @pytest.fixture
 def url():
     url_re = re.compile(r' \* Running on ([^ ]*)')
@@ -37,25 +31,22 @@ def url():
     else:
         server.kill()
         raise Exception("Couldn't get URL from local server")
-    
 
-'''
-Tests for auth.py
-'''
+
 def test_successful_registration(url):
     '''
     Test registration, login and logout working
     '''
-    test_setup.register_user("john@gmail.com", "qwe123!@#", "John", "Smith", url)
-    
-    login_response = test_setup.login_user("john@gmail.com", "qwe123!@#", url)
+    test_setup.auth_register(
+        "john@gmail.com", "qwe123!@#", "John", "Smith", url)
 
-    logout_response = test_setup.logout_user(login_response["token"], url) 
+    login_response = test_setup.auth_login("john@gmail.com", "qwe123!@#", url)
 
-    assert logout_response["is_success"] == True
-    
+    logout_response = test_setup.auth_logout(login_response["token"], url)
+
+    assert logout_response["is_success"]
+
     test_setup.clear(url)
-
 
 
 def test_invalid_email(url):
@@ -63,7 +54,8 @@ def test_invalid_email(url):
     Test that InputError is thrown for invalid email input
     '''
 
-    error_response = test_setup.register_user("john.com", "qwe123!@#" , "John", "Smith", url)
+    error_response = test_setup.auth_register(
+        "john.com", "qwe123!@#", "John", "Smith", url)
 
     assert error_response["code"] == 400
     assert error_response["message"] == "<p>Invalid email</p>"
@@ -77,9 +69,11 @@ def test_email_already_registered(url):
     with email that has already been registered
     '''
 
-    test_setup.register_user("john@gmail.com", "qwe123!@#", "John", "Smith", url)
-    
-    error_response = test_setup.register_user("john@gmail.com", "qwe123!@#", "John", "Smith", url)
+    test_setup.auth_register(
+        "john@gmail.com", "qwe123!@#", "John", "Smith", url)
+
+    error_response = test_setup.auth_register(
+        "john@gmail.com", "qwe123!@#", "John", "Smith", url)
 
     assert error_response["code"] == 400
     assert error_response["message"] == "<p>Email already registered</p>"
@@ -92,14 +86,14 @@ def test_invalid_password(url):
     Test InputError is thrown when length of password entered
     is too short (less than 6 characters)
     '''
-    
-    error_response = test_setup.register_user("john@gmail.com", "qwe",  "John", "Smith", url)
+
+    error_response = test_setup.auth_register(
+        "john@gmail.com", "qwe",  "John", "Smith", url)
 
     assert error_response["code"] == 400
     assert error_response["message"] == "<p>Password too short</p>"
 
     test_setup.clear(url)
-
 
 
 def test_invalid_name_first(url):
@@ -112,14 +106,14 @@ def test_invalid_name_first(url):
                         klashfklashfjklshaklfhasdklfhsadkljfhs
                         adklfhasklhfklsahfklsadhfklasdhfklasdhfkljs"""
 
-    
-    error_response = test_setup.register_user("john@gmail.com","qwe123!@#", long_first_name, "Smith" ,url)
+    error_response = test_setup.auth_register(
+        "john@gmail.com", "qwe123!@#", long_first_name, "Smith", url)
 
     assert error_response["code"] == 400
     assert error_response["message"] == "<p>First Name too long or short</p>"
 
-
-    error_response = test_setup.register_user("john@gmail.com","qwe123!@#", "", "Smith" ,url)
+    error_response = test_setup.auth_register(
+        "john@gmail.com", "qwe123!@#", "", "Smith", url)
 
     assert error_response["code"] == 400
     assert error_response["message"] == "<p>First Name too long or short</p>"
@@ -137,19 +131,19 @@ def test_invalid_name_last(url):
                         klashfklashfjklshaklfhasdklfhsadkljfhs
                         adklfhasklhfklsahfklsadhfklasdhfklasdhfkljs"""
 
-    
-    error_response = test_setup.register_user("john@gmail.com","qwe123!@#", "John", long_last_name ,url)
+    error_response = test_setup.auth_register(
+        "john@gmail.com", "qwe123!@#", "John", long_last_name, url)
 
     assert error_response["code"] == 400
     assert error_response["message"] == "<p>Last Name too long or short</p>"
 
-    error_response = test_setup.register_user("john@gmail.com","qwe123!@#", "John", "" ,url)
+    error_response = test_setup.auth_register(
+        "john@gmail.com", "qwe123!@#", "John", "", url)
 
     assert error_response["code"] == 400
     assert error_response["message"] == "<p>Last Name too long or short</p>"
 
     test_setup.clear(url)
-
 
 
 def test_incorrect_email_login(url):
@@ -158,7 +152,7 @@ def test_incorrect_email_login(url):
     registered yet
     '''
 
-    error_response = test_setup.login_user("bob@gmail.com","qwe123!@#", url)
+    error_response = test_setup.auth_login("bob@gmail.com", "qwe123!@#", url)
 
     assert error_response["code"] == 400
     assert error_response["message"] == "<p>Email does not belong to a user</p>"
@@ -166,15 +160,15 @@ def test_incorrect_email_login(url):
     test_setup.clear(url)
 
 
-
 def test_incorrect_password_login(url):
     '''
     Test that InputError is throw for login where password
     is incorrect
     '''
-    test_setup.register_user("john@gmail.com", "qwe123!@#", "John", "Smith", url)
+    test_setup.auth_register(
+        "john@gmail.com", "qwe123!@#", "John", "Smith", url)
 
-    error_response = test_setup.login_user("john@gmail.com", "qwe12", url)
+    error_response = test_setup.auth_login("john@gmail.com", "qwe12", url)
 
     assert error_response["code"] == 400
     assert error_response["message"] == "<p>Incorrect password</p>"
@@ -188,22 +182,25 @@ def test_logout_fail(url):
     correct error is passed to user
     '''
 
-    test_setup.register_user("john@gmail.com", "qwe123!@#", "John", "Smith", url)
+    test_setup.auth_register(
+        "john@gmail.com", "qwe123!@#", "John", "Smith", url)
 
-    test_setup.login_user("john@gmail.com", "qwe123!@#", url)
+    test_setup.auth_login("john@gmail.com", "qwe123!@#", url)
 
-    logout_response = test_setup.logout_user("WrongToken", url)
+    logout_response = test_setup.auth_logout("WrongToken", url)
 
-    assert logout_response["is_success"] == False
-    
+    assert not logout_response["is_success"]
+
     test_setup.clear(url)
+
 
 def test_handle_too_long(url):
     '''
     Test that handle is being created correctly
     '''
 
-    register_response = test_setup.register_user("john@gmail.com", "qwe123!@#", "1234567890", "yoyoy123456789", url)
+    register_response = test_setup.auth_register(
+        "john@gmail.com", "qwe123!@#", "1234567890", "yoyoy123456789", url)
 
     # response = requests.get(f'{url}users/all', json={"token":register_response["token"]})
 
@@ -214,5 +211,3 @@ def test_handle_too_long(url):
     assert handle == "1234567890yoyoy12345"
 
     test_setup.clear(url)
-
-    
