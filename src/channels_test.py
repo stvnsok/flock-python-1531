05b4c2e0e4e4d70bd40c72fd1e7d060d9ec91ddb
Steven Sok <z5258929@ad.unsw.edu.au>
2020-10-22 17:35:57 +1100

    converting channels_test

--- src/channels_test.py
+++ src/channels_test.py
@@ -8,6 +8,36 @@ import channels
 from data import data 
 from other import clear
 from error import InputError, AccessError
+from subprocess import Popen, PIPE
+import signal
+import requests
+import json
+
+import server
+import helper_test_functions
+
+# Fixture to get the URL of the server. 
+
+@pytest.fixture
+def url():
+    url_re = re.compile(r' \* Running on ([^ ]*)')
+    server = Popen(["python3", "server.py"], stderr=PIPE, stdout=PIPE)
+    line = server.stderr.readline()
+    local_url = url_re.match(line.decode())
+    if local_url:
+        yield local_url.group(1)
+        # Terminate the server
+        server.send_signal(signal.SIGINT)
+        waited = 0
+        while server.poll() is None and waited < 5:
+            sleep(0.1)
+            waited += 0.1
+        if server.poll() is None:
+            server.kill()
+    else:
+        server.kill()
+        raise Exception("Couldn't get URL from local server")
+
 
 #---------------------Testing channels_list function with:---------------------#
 # incorrect token
