53798dcf279f60db9f97544ecb0996a89c709aff
Jay Anand <z5059370@ad.unsw.edu.au>
2020-10-23 12:15:47 +1100

    Fixed bug in message edit and message remove. Was not throwing inputerror if message did not exist. Also checking that if the authorised user has special permissions that allows him to edit/remove a message

--- src/message.py
+++ src/message.py
@@ -2,6 +2,9 @@ from error import InputError, AccessError
 from data import data
 from other import get_timestamp
 import uuid
+from auth import *
+from channels import *
+from other import *
 
 def message_send(token, channel_id, message):
     '''
@@ -56,9 +59,13 @@ def message_remove(token, message_id):
     # Get the channel where the message exists
     channel = next((channel for channel in channels for message in channel['messages'] if message['message_id'] == message_id), None)
 
-    # Check if authorised user is owner
-    channel_owner = any(member['permission_id'] == 1 and member['u_id'] == authorised_user['u_id'] for member in channel['members'])
-    
+    # Raise input error is channel cannot be found
+    if channel is None:
+        raise InputError("Message does not exist")
+
+    # Check if authorised user is channel owner or Flockr owner
+    channel_owner = any((member['is_owner'] and member['u_id'] == authorised_user['u_id']) or (authorised_user['permission_id'] == 1 and member['u_id'] == authorised_user['u_id']) for member in channel['members'])
+
     # Check the messages in the channel
     # Throw an access error if the message was not sent by authorised user, 
     # or authorised user is not an owner of the channel.
@@ -85,26 +92,30 @@ def message_edit(token, message_id, message):
     # Get the channel where the message exists
     channel = next((channel for channel in channels for message in channel['messages'] if message['message_id'] == message_id), None)
 
-    # Check if authorised user is owner
-    channel_owner = any(member['permission_id'] == 1 and member['u_id'] == authorised_user['u_id'] for member in channel['members'])
+    # Raise input error is channel cannot be found
+    if channel is None:
+        raise InputError("Message does not exist")
+
+    # Check if authorised user is channel owner or Flockr owner
+    channel_owner = any((member['is_owner'] and member['u_id'] == authorised_user['u_id']) or (authorised_user['permission_id'] == 1 and member['u_id'] == authorised_user['u_id']) for member in channel['members'])
     
     # Create message
-    updated_message = {
-        'message_id' : str(uuid.uuid4()),
-        'message' : message,
-        'u_id': authorised_user['u_id'],
-        'last_edited': get_timestamp()
-    }
+    # updated_message = {
+    #     'message_id' : str(uuid.uuid4()),
+    #     'message' : message,
+    #     'u_id': authorised_user['u_id'],
+    #     'last_edited': get_timestamp()
+    # }
     
     # If the message is an empty message, delete the message
     if len(message) == 0:   
         channel['messages'].remove(message)
         return {}
     
-    for message in channel['messages']:
-        if message['message_id'] == message_id:
-            if message['u_id'] == authorised_user['u_id'] or channel_owner:
-                message = updated_message
+    for channel_message in channel['messages']:
+        if channel_message['message_id'] == message_id:
+            if channel_message['u_id'] == authorised_user['u_id'] or channel_owner:
+                channel_message['message'] = message
                 return {}
             else:
                 raise AccessError("Message to remove was not sent by authorised user. Authorised user is not an owner of the channel")
