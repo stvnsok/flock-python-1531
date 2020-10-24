7127263b862be9a2c51892261236bf5c4b73e933
Nikhil <z5260565@ad.unsw.edu.au>
2020-10-24 13:10:37 +1100

    changing tests to http

--- src/other_test.py
+++ src/other_test.py
@@ -13,42 +13,43 @@ import signal
 from time import sleep
 import requests
 import helper_test_functions
+from fixture import url as _url
 
 '''
 NEED TO ADD TEST FOR TIMESTAMP
 '''
 ####################################################################################
 
-@pytest.fixture
-def _url():
-    '''
-    Use this fixture to get the URL of the server. It starts the server for you,
-    so you don't need to.
-    '''
-    url_re = re.compile(r' \* Running on ([^ ]*)')
-    server = Popen(["python3", "server.py"], stderr=PIPE, stdout=PIPE)
-    line = server.stderr.readline()
-    local_url = url_re.match(line.decode())
-    if local_url:
-        yield local_url.group(1)
-        # Terminate the server
-        server.send_signal(signal.SIGINT)
-        waited = 0
-        while server.poll() is None and waited < 5:
-            sleep(0.1)
-            waited += 0.1
-        if server.poll() is None:
-            server.kill()
-    else:
-        server.kill()
-        raise Exception("Couldn't get URL from local server")
+# @pytest.fixture
+# def _url():
+#    '''
+#    Use this fixture to get the URL of the server. It starts the server for you,
+#    so you don't need to.
+#    '''
+#    url_re = re.compile(r' \* Running on ([^ ]*)')
+#    server = Popen(["python3", "server.py"], stderr=PIPE, stdout=PIPE)
+#    line = server.stderr.readline()
+#    local_url = url_re.match(line.decode())
+#    if local_url:
+#        yield local_url.group(1)
+#        # Terminate the server
+#        server.send_signal(signal.SIGINT)
+#        waited = 0
+#        while server.poll() is None and waited < 5:
+#            sleep(0.1)
+#            waited += 0.1
+#        if server.poll() is None:
+#            server.kill()
+#    else:
+#        server.kill()
+#        raise Exception("Couldn't get URL from local server")
 
 #####################################################################################
 
 def test_clear(_url):
     # Create new user
     user = helper_test_functions.auth_register('brucewayne@hotmail.com', 'b4tman', 'Bruce', 'Wayne', _url)
-    channels.channels_create(user['token'],"channel", True)
+    helper_test_functions.channels_create(user['token'],"channel", True, _url)
     
     other.clear()
 
