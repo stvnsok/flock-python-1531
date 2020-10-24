2f58f77f1529c74c5850d563ff9762fb24992159
Nikhil <z5260565@ad.unsw.edu.au>
2020-10-24 23:41:53 +1100

    finished extra tests

--- src/other_test.py
+++ src/other_test.py
@@ -1,8 +1,4 @@
 from data import data
-import auth
-import channel
-import channels
-import message
 import other
 from error import InputError, AccessError
 from datetime import datetime
@@ -14,37 +10,6 @@ from time import sleep
 import requests
 import helper_test_functions
 from fixture import url as _url
-
-'''
-NEED TO ADD TEST FOR TIMESTAMP
-'''
-####################################################################################
-
-# @pytest.fixture
-# def _url():
-#    '''
-#    Use this fixture to get the URL of the server. It starts the server for you,
-#    so you don't need to.
-#    '''
-#    url_re = re.compile(r' \* Running on ([^ ]*)')
-#    server = Popen(["python3", "server.py"], stderr=PIPE, stdout=PIPE)
-#    line = server.stderr.readline()
-#    local_url = url_re.match(line.decode())
-#    if local_url:
-#        yield local_url.group(1)
-#        # Terminate the server
-#        server.send_signal(signal.SIGINT)
-#        waited = 0
-#        while server.poll() is None and waited < 5:
-#            sleep(0.1)
-#            waited += 0.1
-#        if server.poll() is None:
-#            server.kill()
-#    else:
-#        server.kill()
-#        raise Exception("Couldn't get URL from local server")
-
-#####################################################################################
 
 def test_clear(_url):
     # Create new user
@@ -67,7 +32,6 @@ def test_users_all(_url):
     assert other.users_all(token) == {'users': data['users']}
 
     other.clear()
-
 
 def test_admin_userpermission_change(_url):
     # Testing if permission changes are correctly executed
@@ -95,7 +59,6 @@ def test_admin_userpermission_change(_url):
 
     other.clear()
 
-
 def test_admin_userpermission_change_invalid_permission_id(_url):
     # Testing if permission changes catch invalid permission ids
     user = helper_test_functions.auth_register('brucewayne@hotmail.com', 'b4tman', 'Bruce', 'Wayne', _url)
@@ -115,12 +78,10 @@ def test_admin_userpermission_change_invalid_permission_id(_url):
 
     # Change permissions to failure
     error = helper_test_functions.change_userpermission(user['token'], user2['u_id'], 3, _url)
-
     assert error['code'] == 400
     assert error['message'] == '<p>Permission_id does not refer to a value permission</p>'
 
     other.clear()
-
 
 def test_admin_userpermission_change_invalid_admin(_url):
     # Testing if permission changes catch invalid admins
@@ -141,35 +102,35 @@ def test_admin_userpermission_change_invalid_admin(_url):
 
     # Change permissions to failure
     error = helper_test_functions.change_userpermission(user2['token'], user['u_id'], 1, _url)
-
     assert error['code'] == 400
     assert error['message'] == '<p>The authorised user is not an admin or owner</p>'
 
     other.clear()
 
-'''
 def test_admin_userpermission_change_invalid_user(_url):
     # Testing if permission changes catch invalid users
-    user = auth.auth_register('brucewayne@hotmail.com', 'b4tman', 'Bruce', 'Wayne')
-    users = data['users']
+    user = helper_test_functions.auth_register('brucewayne@hotmail.com', 'b4tman', 'Bruce', 'Wayne', _url)
     token = user['token']
-    
-    authorised_user = next((user for user in users if user['token'] == token), None)
-    assert authorised_user['permission_id'] == 1
+    users = helper_test_functions.users_all(token, _url)
 
-    user2 = auth.auth_register('jacknapier@hotmail.com', 'j0kerr', 'Jack', 'Napier') 
+    # Check if first user permissions are admin permissions (1)
+    assert users['users'][0]['permission_id'] == 1
+
+    # Register new user
+    user2 = helper_test_functions.auth_register('jacknapier@hotmail.com', 'j0kerr', 'Jack', 'Napier', _url) 
     token2 = user2['token']
+    users = helper_test_functions.users_all(token2, _url)
 
-    authorised_user2 = next((user for user in users if user['token'] == token2), None)
-    assert authorised_user2['permission_id'] == 2
-    
-    with pytest.raises(InputError) as e:
-        other.admin_userpermission_change(user['token'], 'bat', 2)
-    assert '400 Bad Request: U_id does not refer to a valid user' == str(e.value)
+    # Check if first user permissions are regular permissions (2)
+    assert users['users'][1]['permission_id'] == 2
+
+    # Change permissions to failure
+    error = helper_test_functions.change_userpermission(user['token'], 'bat', 1, _url)
+    assert error['code'] == 400
+    assert error['message'] == '<p>U_id does not refer to a valid user</p>'
 
     other.clear()
 
-'''
 def test_other_search(_url):
     # Register new user
     user = helper_test_functions.auth_register('brucewayne@hotmail.com', 'b4tman', 'Bruce', 'Wayne', _url)
