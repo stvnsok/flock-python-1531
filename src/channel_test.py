14c0ce0d20297d7963da0dc74214b1bb4a9ae876
Steven Sok <z5258929@ad.unsw.edu.au>
2020-10-25 14:49:06 +1100

    comment

--- src/channel_test.py
+++ src/channel_test.py
@@ -16,30 +16,31 @@ from error import InputError, AccessError
 from other import clear
 from fixture import url as _url
 
-@pytest.fixture
-def _url():
-    '''
-    Use this fixture to get the URL of the server. It starts the server for you,
-    so you don't need to.
-    '''
 
-    url_re = re.compile(r' \* Running on ([^ ]*)')
-    server = Popen(["python3", "server.py"], stderr=PIPE, stdout=PIPE)
-    line = server.stderr.readline()
-    local_url = url_re.match(line.decode())
-    if local_url:
-        yield local_url.group(1)
-        # Terminate the server
-        server.send_signal(signal.SIGINT)
-        waited = 0
-        while server.poll() is None and waited < 5:
-            sleep(0.1)
-            waited += 0.1
-        if server.poll() is None:
-            server.kill()
-    else:
-        server.kill()
-        raise Exception("Couldn't get URL from local server")
+#@pytest.fixture
+#def _url():
+#    '''
+#    Use this fixture to get the URL of the server. It starts the server for you,
+#    so you don't need to.
+#    '''
+
+#    url_re = re.compile(r' \* Running on ([^ ]*)')
+#    server = Popen(["python3", "server.py"], stderr=PIPE, stdout=PIPE)
+#    line = server.stderr.readline()
+#    local_url = url_re.match(line.decode())
+#    if local_url:
+#        yield local_url.group(1)
+#        # Terminate the server
+#        server.send_signal(signal.SIGINT)
+#        waited = 0
+#        while server.poll() is None and waited < 5:
+#            sleep(0.1)
+#            waited += 0.1
+#        if server.poll() is None:
+#            server.kill()
+#    else:
+#        server.kill()
+#        raise Exception("Couldn't get URL from local server")
 
 ######################### Tests for channel/invite #############################
 def test_channel_invite_token_incorrect(_url):
