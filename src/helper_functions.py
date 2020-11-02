0bed480c8cbdf07af512037eeb89d176a7145488
Jay Anand <z5059370@ad.unsw.edu.au>
2020-11-02 23:00:01 +1100

    Refactored message_remove, message_send with helper_functions

--- src/helper_functions.py
+++ src/helper_functions.py
@@ -3,18 +3,22 @@ Set of helper functions that help reduce code within actual features
 themselves
 '''
 
+from data import data
+
+def get_authorised_user(token):
+    return next((user for user in data['users'] if user['token'] == token), None)
+
 def get_message(message_id, channel):
     '''
     Helper function that allows a message to be retrieved from a channel.
     Assumes message does exist and previous exceptions have been handled.
     '''
-
+    
     for message in channel['messages']:
         if message['message_id'] == message_id:
             return message
     
     
-
 def update_message(message_id, new_message, channel):
     '''
     Helper function to update a message quickly.
@@ -27,26 +31,28 @@ def update_message(message_id, new_message, channel):
             return 
 
 
-
-def get_channel(token, message_id):
+def get_channel_with_message(message_id):
     '''
     Get the channel where the given message is.
     '''    
-    channels = data['channels']
     
-    # Get the channel where the message exists
-    channel = next((channel for channel in channels for message in channel['messages'] if message['message_id'] == message_id), None)
+    return next((channel for channel in data['channels'] for message in channel['messages'] if message['message_id'] == message_id), None)
 
-    # Raise input error is channel cannot be found
-    if channel is None:
-        return -1
 
-    return channel
 
 def channel_member(token, channel):
     '''
     See if a user is part of a channel
     '''
-    authorised_user = next((user for user in data['users'] if user['token'] == token), None)
+    
+    user = next((user for user in data['users'] if user['token'] == token), None)
 
     return any(user['u_id'] == member['u_id'] for member in channel['members'])
+
+
+def get_channel(token, channel_id):
+    '''
+    Get the channel associated with the given channel_id
+    '''
+    
+    return next((channel for channel in data['channels'] if channel['channel_id'] == channel_id), None)
--- src/message.py
+++ src/message.py
@@ -2,8 +2,7 @@ from error import InputError, AccessError
 from data import data
 from other import get_timestamp
 import uuid
-from helper_functions import get_message, get_channel, update_message, channel_member
-
+import helper_functions 
 
 
 def message_send(token, channel_id, message):
@@ -11,21 +10,16 @@ def message_send(token, channel_id, message):
     Send a message from the authorised_user to the channel specified by channel_id 
     '''
 
-    users = data['users']
-    channels = data['channels']
-
     # Get the user that is sending the request
-    authorised_user = next(
-        (user for user in users if user['token'] == token), None)
+    authorised_user = helper_functions.get_authorised_user(token)
     
     # Get the selected channel
-    channel = next((channel for channel in channels if channel['channel_id'] == channel_id), None)
-
-    # Check if authorised user is a member of the channel
-    if not any(authorised_user['u_id'] == member['u_id'] for member in channel['members']):
-        raise AccessError(
-            'Authorised user has not joined this channel yet')    
+    channel = helper_functions.get_channel(token, channel_id)
     
+    # Check if authorised user is a member of the channel    
+    if not helper_functions.channel_member(token, channel):
+        raise AccessError('Authorised user has not joined this channel yet')
+
     # Check if message is too long
     if len(message) > 1000:
         raise InputError("Message is more than 1000 characters")
@@ -49,33 +43,27 @@ def message_remove(token, message_id):
     '''
     Given a message_id for a message, this message is removed from the channel
     '''
-    users = data['users']
-    channels = data['channels']
-    
-    # Get the user that is sending the request
-    authorised_user = next(
-        (user for user in users if user['token'] == token), None)
+
+    authorised_user = helper_functions.get_authorised_user(token)
 
     # Get the channel where the message exists
-    channel = next((channel for channel in channels for message in channel['messages'] if message['message_id'] == message_id), None)
-
+    channel = helper_functions.get_channel_with_message(message_id)
     # Raise input error is channel cannot be found
     if channel is None:
         raise InputError("Message does not exist")
 
     # Check if authorised user is channel owner or Flockr owner
     channel_owner = any((member['is_owner'] and member['u_id'] == authorised_user['u_id']) or (authorised_user['permission_id'] == 1 and member['u_id'] == authorised_user['u_id']) for member in channel['members'])
-
+    
     # Check the messages in the channel
     # Throw an access error if the message was not sent by authorised user, 
     # or authorised user is not an owner of the channel.
-    for message in channel['messages']:
-        if message['message_id'] == message_id:
-            if message['u_id'] == authorised_user['u_id'] or channel_owner:
-                channel['messages'].remove(message)
-                return {}
-            else:
-                raise AccessError("Message to remove was not sent by authorised user. Authorised user is not an owner of the channel")
+    message = helper_functions.get_message(message_id, channel)
+    if message['u_id'] == authorised_user['u_id'] or channel_owner:
+        channel['messages'].remove(message)
+        return {}
+    else:
+        raise AccessError("Message to remove was not sent by authorised user. Authorised user is not an owner of the channel")
     
 
 def message_edit(token, message_id, message):
