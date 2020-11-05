1d901fe500eeb7db1ba4589c6ec1c34cf3888682
Steven Sok <z5258929@ad.unsw.edu.au>
2020-11-06 01:26:53 +1100

    created testing file for standup

---
+++ src/standup_test.py
@@ -0,0 +1,47 @@
+'''
+6/11/2020
+
+'''
+import helper_test_functions
+from fixture import url
+
+'''
+######################## Tests for standup/start #############################
+def test_standup_start_token_incorrect(url) {
+
+}
+
+def test_standup_start_invalid_channel_id(url){
+}
+
+def test_standup_start_invalid_length(url) {
+}
+
+def test_standup_start_working(url) {
+}
+
+######################## Tests for standup/active #############################
+def test_standup_active_token_incorrect(url) {
+
+}
+
+def test_standup_active_invalid_channel_id(url){
+}
+
+def test_standup_active_working(url) {
+}
+
+
+######################## Tests for standup/send #############################
+
+def test_standup_send_token_incorrect(url) {
+
+}
+
+def test_standup_send_invalid_channel_id(url){
+}
+
+def test_standup_send_working(url) {
+}
+
+'''
