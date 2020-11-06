c55c9c7049a3cd354b1a94028713746467de78ba
Steven Sok <z5258929@ad.unsw.edu.au>
2020-11-06 21:04:11 +1100

    added some tests for standup/active

--- src/standup_test.py
+++ src/standup_test.py
@@ -56,20 +56,53 @@ def test_standup_start_standup_active(url):
 
 ######################## Tests for standup/active #############################
 def test_standup_active_token_incorrect(url):
+    user_1 = helper_test_functions.auth_register(
+        "123@hotmail.com",
+        "password",
+        "Bobby",
+        "McBob",
+        url
+    )
+    
+    response = helper_test_functions.channels_create(token_1, 'channel_1', True, url)
+    new_channel = response
+    channel_id = new_channel['channel_id']
+    
+    error = helper_test_functions.standup_active(user_1['token'],channel_id, 10,  url)
+    assert error['code'] == 400
+    assert error['message'] == '<p>Token is incorrect</p>'
 
+    helper_test_functions.clear(url)
 
 
 def test_standup_active_invalid_channel_id(url):
 
+    user_1 = helper_test_functions.auth_register(
+        "123@hotmail.com",
+        "password",
+        "Bobby",
+        "McBob",
+        url
+    )
+
+    
+    error = helper_test_functions.standup_active(user_1['token'], 1, 10,  url)
+    assert error['code'] == 400
+    assert error['message'] == '<p>Channel_id does not exist</p>'
+
+    helper_test_functions.clear(url)
+
 
 def test_standup_active_working(url): 
+    
+    
 
 
 
 ######################## Tests for standup/send #############################
 
 def test_standup_send_token_incorrect(url): 
-    
+        
 
 
 def test_standup_send_invalid_channel_id(url):
